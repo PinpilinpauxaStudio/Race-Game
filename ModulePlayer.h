a5511e283ab065a0a56b11092d0fd8f90fecbee7
HugoUPC <hugo.planell@estudiantat.upc.edu>
2024-01-13 19:23:33 +0100

    map changes

--- ModulePlayer.h
+++ ModulePlayer.h
@@ -5,7 +5,7 @@
 
 struct PhysVehicle3D;
 
-#define MAX_ACCELERATION 1000.0f
+#define MAX_ACCELERATION 1500.0f
 #define TURN_DEGREES 45.0f * DEGTORAD
 #define BRAKE_POWER 50.0f
 
--- map/blender_coords_to_txt.py
+++ map/blender_coords_to_txt.py
@@ -1,15 +1,15 @@
 import bpy
 
-output_path = r"C:\Users\hugopm\Desktop\coords_out.txt"
+output_path = r"C:\Users\Hugo\Documents\GitHub\Race-Game\map\coords_out.txt"
 
 with open(output_path, "w") as file:
     for ob in bpy.context.scene.objects:
         mat = ob.matrix_world
-        loc = mat.translation * 10  # Multiply by 100 for scaling
+        loc = mat.translation / 2  # Multiply by 100 for scaling
         loc.x *= -1  # Invert X-axis
         
         rot = mat.to_euler()
-        scale = ob.scale * 20
+        scale = ob.scale
         
         # Formatting the output line
         output_line = f'sog.CreateRectangle({{{loc.x:.2f}, {loc.z:.2f}, {loc.y:.2f}}}, ' \
